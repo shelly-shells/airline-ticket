a51810bd1a593c542f865ddf0c6d94cffa68d3e4
MBUYt0n <shusrith03@gmail.com>
2024-09-01 15:48:38 +0530

    .

--- a.py
+++ a.py
@@ -7,13 +7,10 @@ a = df["fltno"]
 
 d = []
 for i in a:
-    x, y = i.split(" ")
+    x, y = i.split()
     url = f"https://www.flightstats.com/v2/flight-tracker/{x}/{y}"  # Replace with the actual URL
     response = requests.get(url)
-
-    # Parse the page content with BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
-    # soup = soup.prettify()
 
     id = soup.find_all("div", class_="text-helper__TextHelper-sc-8bko4a-0 OvgJa")
     dep = soup.find_all("a", class_="route-with-plane__AirportLink-sc-154xj1h-3 kCdJkI")
---
+++ b.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+df = pd.read_csv("output.csv")
+a = df["id"]
+
+d = []
+j = 0
+for i in a:
+    x, y = i.split()
+    url = f"https://www.flightstats.com/v2/flight-tracker/{x}/{y}"  # Replace with the actual URL
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    dist = soup.find_all("h5", class_="labeled-columns__Value-sc-j3eq63-1 bKkaUj")
+    a = [i.get_text() for i in dist]
+    d.append([a[0], a[-2], a[-1]])
+    j += 1
+    if j % 500 == 0:
+        print(j)
+
+df1 = pd.DataFrame(d, columns=["Distance", "AircraftCode", "Aircraft"])
+df["Distance"] = df1["Distance"]
+df["AircraftCode"] = df1["AircraftCode"]
+df["Aircraft"] = df1["Aircraft"]
+df.to_csv("output.csv", index=False)
+
