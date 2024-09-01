6562abe1b71df41691a289f19cf6ea20452b04fb
MBUYt0n <shusrith03@gmail.com>
2024-09-01 18:29:51 +0530

    .

--- b.py
+++ b.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 df = pd.read_csv("output.csv")
-a = df["id"]
+a = ["AI 776"]
 
 d = []
 j = 0
@@ -15,14 +15,15 @@ for i in a:
     soup = BeautifulSoup(response.text, "html.parser")
     dist = soup.find_all("h5", class_="labeled-columns__Value-sc-j3eq63-1 bKkaUj")
     a = [i.get_text() for i in dist]
-    d.append([a[0], a[-2], a[-1]])
-    j += 1
-    if j % 500 == 0:
-        print(j)
+    # d.append([a[0], a[-2], a[-1]])
+    # j += 1
+    # if j % 500 == 0:
+    #     print(j)
+    print(a)
 
-df1 = pd.DataFrame(d, columns=["Distance", "AircraftCode", "Aircraft"])
-df["Distance"] = df1["Distance"]
-df["AircraftCode"] = df1["AircraftCode"]
-df["Aircraft"] = df1["Aircraft"]
-df.to_csv("output.csv", index=False)
+# df1 = pd.DataFrame(d, columns=["Distance", "AircraftCode", "Aircraft"])
+# df["Distance"] = df1["Distance"]
+# df["AircraftCode"] = df1["AircraftCode"]
+# df["Aircraft"] = df1["Aircraft"]
+# df.to_csv("output.csv", index=False)
 
