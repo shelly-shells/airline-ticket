e07cb45169986fe8756a853a0ac483e6c5b03e4a
MBUYt0n <shusrith03@gmail.com>
2024-09-08 13:05:43 +0530

    .

---
+++ dataset-making/fixes.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("csvs/planes.csv")
+df1 = pd.read_csv("csvs/routes.csv")
+
+
+a = df["AircraftCode"]
+df1["airline_id"] = a
+df1.to_csv("csvs/routes.csv", index=False)
+
+df = df.drop(["id", "airline"], axis=1)
+df = df.drop_duplicates()
+df.to_csv("csvs/planes.csv", index=False)
