0ef4d8c8728096c7cd60e0a6d949a906d74d3258
MBUYt0n <shusrith03@gmail.com>
2024-09-28 18:36:28 +0530

    bookings

--- dataset-making/bookings.py
+++ dataset-making/bookings.py
@@ -26,6 +26,7 @@ passengers = [
 children = [random.randint(0, i // 2) for i in passengers]
 passengers = [i - j for i, j in zip(passengers, children)]
 fid = [[x[0], x[1], random.choice([i for i in range(7) if x[i + 2] == 1])] for x in fid]
+fid = [[x[0], x[1], random.choice(weekdays[x[2]])] for x in fid]
 bookings = [
     [random.choice(users), *random.choice(fid), passengers[i], children[i]]
     for i in range(500)
@@ -33,4 +34,28 @@ bookings = [
 bookings = pd.DataFrame(
     bookings, columns=["username", "fid", "base_price", "day", "passengers", "children"]
 )
-bookings.to_csv("a.csv", index=False)
+bookings["price"] = (
+    bookings["base_price"] * bookings["passengers"]
+    + bookings["base_price"] * 0.5 * bookings["children"]
+)
+bookings.drop(columns=["base_price"], inplace=True)
+bookings["food"] = [
+    random.choices([0, 1], weights=[0.75, 0.25], k=1)[0] for i in range(500)
+]
+bookings["luggage"] = [
+    random.choices([0, 1], weights=[0.75, 0.25], k=1)[0] for i in range(500)
+]
+start_date = datetime(2024, 3, 1)
+end_date = datetime(2024, 7, 31)
+
+
+def random_timestamp(start_date, end_date):
+    start_timestamp = start_date.timestamp()
+    end_timestamp = end_date.timestamp()
+    random_timestamp = random.uniform(start_timestamp, end_timestamp)
+    return datetime.fromtimestamp(random_timestamp).strftime("%Y-%m-%d %H:%M:%S")
+
+bookings["timestamp"] = [random_timestamp(start_date, end_date) for i in range(500)]
+
+
+bookings.to_csv("csvs/bookings.csv", index=False)
