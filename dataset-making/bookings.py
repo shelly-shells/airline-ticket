a9ddc2f70180bf047016a4040868d35362c50596
MBUYt0n <shusrith03@gmail.com>
2024-09-28 08:59:55 +0530

    .

--- dataset-making/bookings.py
+++ dataset-making/bookings.py
@@ -2,17 +2,18 @@ import pandas as pd
 import random
 from datetime import datetime, timedelta
 
-# df = pd.read_csv("csvs/users.csv")
-# df1 = pd.read_csv("csvs/routes.csv")
+df = pd.read_csv("csvs/users.csv")
+df1 = pd.read_csv("csvs/routes.csv")
 
-# book = df.sample(random.randint(20, 45))
-# book["route_id"] = df1.sample(len(book))["route_id"]
 
 start = datetime(2024, 9, 1)
-end = datetime(2024, 9, 8)
-
+end = datetime(2024, 10, 31)
+weekdays = {}
 while start <= end:
+    if start.weekday() not in weekdays:
+        weekdays[start.weekday()] = []
+    weekdays[start.weekday()].append(start.strftime("%Y-%m-%d"))
     start += timedelta(days=1)
-    print(start)
-    if start.weekday() == 0:
-        print(start.strftime("%Y-%m-%d"))
+
+users = df1[df1["role"] == "user"]["username"].tolist()
+fid = df["id"].tolist()
