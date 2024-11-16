fd607e80ca664f2dc304c4949c05a086f939f22d
MBUYt0n <shusrith03@gmail.com>
2024-11-16 20:51:12 +0530

    fixes

--- sqlConnector/price_plot.py
+++ sqlConnector/price_plot.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from pricing import get_price
 import plotly.graph_objects as go
+
+
+def getPrice(basePrice, seatProportion, daysLeft):
+    daysLeft *= 100
+    dateCoeff = (basePrice * np.exp(0.01 * (1 - (0.02 * daysLeft))) / 300) + 1
+    seatCoeff = 0.7 - 3 * np.log10((seatProportion) + 0.1)
+    return basePrice * dateCoeff * seatCoeff / (basePrice // 1000) ** 1.5
+
 
 bp = 2000
 l = [(2000 * np.exp(0.01 * (1 - (0.02 * i)))) for i in range(0, 30000, 100)]
@@ -12,14 +19,14 @@ plt.plot(l)
 
 l = [(0.7 - 3 * np.log10((i / 100) + 0.1)) for i in range(0, 100)]
 plt.plot(l)
-plt.savefig("plot2.png")
+# plt.savefig("plot2.png")
 
 basePrice = 4000
 seatProportions = np.linspace(0, 1, 100)
 daysLeft = np.linspace(1, 300, 100)
 
 seatProportionGrid, daysLeftGrid = np.meshgrid(seatProportions, daysLeft)
-priceGrid = get_price(basePrice, seatProportionGrid, daysLeftGrid / 100)
+priceGrid = getPrice(basePrice, seatProportionGrid, daysLeftGrid / 100)
 
 fig = go.Figure(
     data=[
